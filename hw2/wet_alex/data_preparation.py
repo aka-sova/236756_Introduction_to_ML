a15a0fb02737511c6b3b17fe8084c85cc07980bb
Alexander Shender <aka.sova@gmail.com>
2020-11-16 23:34:09 +0200

    upload some analysis on the dataset

---
+++ hw2/wet_alex/data_preparation.py
@@ -0,0 +1,37 @@
+
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+
+
+
+
+def main():
+
+    # 1. load the csv input
+    virus_df = pd.read_csv("virus_hw2.csv")
+
+    train_ds_part = 0.75
+    valid_ds_part = 0.15
+    test_ds_part = 0.10
+
+    virus_df.iloc()
+
+    virus_df["BMI"].isna
+
+
+
+
+
+    pass
+
+
+
+
+if __name__ == "__main__":
+
+    main()
+
